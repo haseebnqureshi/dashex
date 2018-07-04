34094b927832cef052487cb55ac27f305123d01b
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2018-07-04 11:34:00 -0400

    bumped version

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var exec = require('child_process').execSync;
-var from = __dirname + '/dashex/';
-var to = `${process.env.PWD}/dashex/`;
-exec(`cp -R ${from} ${to}`);
+try {
+	var exec = require('child_process').execSync;
+	var path = require('path');
+	var from = path.resolve(__dirname, '/dashex/');
+	var to = path.resolve('..', __dirname, '/dashex/');
+	exec(`cp -R ${from} ${to}`);
+}
+catch (err) {
+	console.log('dashexpress postinstall.js', err);
+}
