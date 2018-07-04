8fae8b5e4928f96bfc5a6aad65fe76bf4185ceb9
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2018-07-04 11:29:37 -0400

    corrected lingering typo with dashex directories; bumped version

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var exec = require('child_process').execSync;
-var from = __dirname + '/dashdb/';
-var to = `${process.env.PWD}/dashdb/`;
+var from = __dirname + '/dashex/';
+var to = `${process.env.PWD}/dashex/`;
 exec(`cp -R ${from} ${to}`);
